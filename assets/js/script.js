a5e8d0dee3b43e9561b46a19cbd534ba47df2892
sonictrain <nicola.brucoli92@gmail.com>
2024-01-14 15:50:35 +0000

    added event listener for clicks on the search button which checks for the input field - displaying the invalid feedback accordingly - add the search to the search history, runs the API call filtering out unique result by country, and display the unique results in the dashboard for the user to pick a different location in case the first result is not accurate.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const removeRecentBtn = `<button type="button" class="btn btn-outline-secondary btn-sm ms-2 remove-item-btn">Delete</button>`;
 const recentDropdown = $('#dropdown-recent');
-let recentLocations = ['London','Hong Kong', 'Lisbon'];
+const searchBtn = $('#search-btn');
+let recentLocations = [];
 
 dropDownRecent();
 
@@ -16,7 +17,7 @@ function dropDownRecent() {
     const clearAllBtn = $('<button>').addClass('btn btn-secondary w-100 rounded-bottom').text('Clear search history').append(bsIconGenerator('bi bi-trash ps-1'));
     const clearAllEl = $(listItem).append(clearAllBtn);
 
-    clearAllEl.on('click', function() {
+    clearAllEl.on('click', function () {
         if (recentLocations.length) {
             recentLocations = [];
             dropDownRecent();
@@ -24,16 +25,16 @@ function dropDownRecent() {
     });
 
     if (recentLocations.length) {
-        
-        $.each(recentLocations, function( i, location) {
+
+        $.each(recentLocations, function (i, location) {
             const dropDownLi = $('<li>').attr('class', 'd-flex px-2');
             const divRecent = $('<button>').attr('class', 'dropdown-item rounded');
             $(dropDownLi).addClass('recent-item').append(divRecent.text(location));
             $(dropDownLi).hover(
                 function () {
-                    $(this).append($(removeRecentBtn)).on('click',function(e) {
+                    $(this).append($(removeRecentBtn)).on('click', function (e) {
                         let toClear = $(e.target).siblings('.dropdown-item').text();
-                        recentLocations = recentLocations.filter(function(x) {
+                        recentLocations = recentLocations.filter(function (x) {
                             return x !== toClear
                         });
                         dropDownRecent();
@@ -48,11 +49,73 @@ function dropDownRecent() {
     } else {
         const dropDownLi = $('<li>').attr('class', 'd-flex px-2');
         const divRecent = $('<button>').attr('class', 'dropdown-item rounded');
-        $(dropDownLi).append(divRecent.addClass('disabled').attr('aria-disabled','true').text('Empty'));
+        $(dropDownLi).append(divRecent.addClass('disabled').attr('aria-disabled', 'true').text('Empty'));
         $(recentDropdown).prepend(dropDownLi);
         $(clearAllBtn).addClass('disabled');
     }
     $(recentDropdown)
         .append(dropDownDivider)
         .append(clearAllEl);
+};
+
+// click on search button event listener 
+searchBtn.on('click', (e) => {
+
+    e.preventDefault();
+
+    const inputCity = $('#inputCity').val().trim();
+    const apiKey = `748b56884fc69047f189d030cf1dd596`;
+
+    const inputValidation = $('.input-validation');
+
+    // check if input field is empty
+    if (inputCity) {
+
+        // if yes, display invalid feedback
+        $(inputValidation).each((i, x) => { $(x).removeClass('is-invalid') });
+
+        // if location is not yet in the recent dropdown add it and refresh the list
+        if (!recentLocations.includes(inputCity)) {
+            recentLocations.push(inputCity);
+            dropDownRecent();
+        } else {
+            // if already there move it on top of the dropdown
+            recentLocations.push(recentLocations.splice(recentLocations.indexOf(inputCity), 1)[0]);
+            dropDownRecent();
+        }
+
+        const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=5&appid=${apiKey}`;
+
+        // API Call to get the best 5 result
+        fetch(geoURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                
+                // filter out the array and keep one unique result per country
+                let uniqueCountry = []
+                data = jQuery.map((data), function( obj, i) {
+                    if (!uniqueCountry.includes(obj.country)) {
+                        uniqueCountry.push(obj.country);
+                        return obj;
+                    }
+                })
+                console.log(data);
+                renderResults(data);
+            });
+
+    } else {
+        // if input field is empty show invalid feedback
+        $(inputValidation).each((i, x) => { $(x).addClass('is-invalid') });
+    }
+
+});
+
+// render the best unique results in the dashboard (up to 5 result)
+function renderResults(objArr) {
+    const resultsContainer = $('#results-container').empty();
+    $(objArr).each((i, r) => {
+        resultsContainer.append($('<button>').addClass('btn btn-outline-secondary').text(`${r.name}, ${r.country}`));
+    })
 }
