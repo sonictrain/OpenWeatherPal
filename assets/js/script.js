54b7ccc7ad8eb31190409cd2c664c155d2f17230
sonictrain <nicola.brucoli92@gmail.com>
2024-01-17 02:30:18 +0000

    added jquery function to append script tag with a builtin function to make mapbox working, the function is refreshed everytime the user perform a location search. added boilerplate for the second api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
+
 const removeRecentBtn = `<button type="button" class="btn btn-outline-secondary btn-sm ms-2 remove-item-btn">Delete</button>`;
 const recentDropdown = $('#dropdown-recent');
 const searchBtn = $('#search-btn');
 let recentLocations = [];
 
 dropDownRecent();
+
+$('body').append(`<script id="mapBox">updateMap([-0.15, 51.5]);
+
+function updateMap(coordinate) {
+mapboxgl.accessToken = 'pk.eyJ1Ijoic29uaWN0cmFpbiIsImEiOiJjaWpvaXpxaXMwMHdmdW9seGVybXc2NGJtIn0.5B0Jv5pJCNYs9pNdl3tyQA';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: coordinate, // starting position [lng, lat]
+    zoom: 9, // starting zoom
+});
+const marker1 = new mapboxgl.Marker()
+.setLngLat(coordinate)
+.addTo(map);
+};</script>`)
+
 
 function bsIconGenerator(bs_className) {
     return `<i class="${bs_className}"></i>`;
@@ -77,7 +93,7 @@ function renderResults(objArr) {
 
     const resultsContainer = $('#results-container').empty();
     if (objArr.length > 1) {
-        $(resultsContainer).append($('<p>').addClass('m-0').text('Did you mean:'))
+        $(resultsContainer).append($('<p>').addClass('m-0 fs-6').text('Did you mean:'))
     };
 
     $(objArr.splice(1)).each((i, r) => {
@@ -102,6 +118,7 @@ function searchLocation(locationString) {
 
         // if not, remove invalid feedback
         $(inputValidation).each((i, x) => { $(x).removeClass('is-invalid') });
+        $('#input-label').removeClass('text-danger').text('City');
 
         const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${locationString}&limit=5&appid=${apiKey}`;
 
@@ -126,37 +143,81 @@ function searchLocation(locationString) {
                 // if location is not yet in the recent dropdown add it and refresh the list
                 if (!recentLocations.includes(`${data[0].name}, ${data[0].country}`)) {
                     recentLocations.push(`${data[0].name}, ${data[0].country}`);
+                    $('#inputCity').val(`${data[0].name}, ${data[0].country}`);
                     dropDownRecent();
-                    
+
                 } else {
                     // if already there move it on top of the dropdown
                     recentLocations.push(recentLocations.splice(recentLocations.indexOf(`${data[0].name}, ${data[0].country}`), 1)[0]);
                     dropDownRecent();
                 }
 
+                // close the sidebar
                 $('#offcanvasScrolling').removeClass('show');
 
+                // empty the current-weather container
                 $('#current-weather').empty();
+                // create and append the location name
                 createCard($('#current-weather'), `Currently in ${data[0].name}, ${data[0].country}:`);
-                
-            });
+
+                // save lat & lon
+                const lat = data[0].lat;
+                const lon = data[0].lon;
+
+                $('#mapBox').empty();
+                $('body').append(`<script id="mapBox">updateMap([${lon}, ${lat}]);
+                function updateMap(coordinate) {
+                mapboxgl.accessToken = 'pk.eyJ1Ijoic29uaWN0cmFpbiIsImEiOiJjaWpvaXpxaXMwMHdmdW9seGVybXc2NGJtIn0.5B0Jv5pJCNYs9pNdl3tyQA';
+                const map = new mapboxgl.Map({
+                    container: 'map', // container ID
+                    center: coordinate, // starting position [lng, lat]
+                    zoom: 9, // starting zoom
+                });
+                const marker1 = new mapboxgl.Marker()
+                .setLngLat(coordinate)
+                .addTo(map);
+                };</script>`);
+
+                const currentURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+                // run API to get weather information
+                fetch(currentURL)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log(data);
+
+                        // display the weather icon
+                        let weatherIcon = data.weather[0].id;
+                        $('#current-weather').append($('<img>').addClass('wi-icon-md').attr('src', `./assets/images/svg/${iconMap["openweathermap/codes/"][weatherIcon]}.svg`));
+                    })
+                    // console log the error if any
+                    .catch(error => console.log(error));
+
+
+            })
+            // console log the error if any
+            .catch(error => console.log(error));
 
     } else {
         // if input field is empty show invalid feedback
         $(inputValidation).each((i, x) => { $(x).addClass('is-invalid') });
+        $('#input-label').addClass('text-danger').text('Invalid city');
+
     }
 };
 
-
+// create card function - it needs parent id/class using the selectors (# or .) and a title for the card
 function createCard(parent, title) {
     let col = $('<div>').addClass('col');
     let card = $('<div>').addClass('card h-100 border-0 rounded-4 bg-body-secondary');
     let cardBody = $('<div>').addClass('card-body');
     let cardTitle = $('<h5>').addClass('card-title').text(title);
-    
+
     $(cardBody).append(cardTitle);
     $(card).append(cardBody);
     $(col).append(card);
-    
+
     $(parent).append(col);
-}
+};
